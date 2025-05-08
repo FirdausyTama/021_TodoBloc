37238bfbd01294c4b4b83c1f7bdab662512fdf8e
FirdausyTama <atama.firdausy@gmail.com>
2025-05-08 17:26:09 +0700

    membuat container

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/bloc/todo_bloc.dart';
-import 'package:todo_app/model/todo.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
@@ -108,6 +107,15 @@ class TodoPage extends StatelessWidget {
                       if (state.todos.isEmpty) {
                         return Center(child: Text('Todo list is empty'));
                       }
+                      return ListView.builder(
+                        itemCount: state.todos.length,
+                        itemBuilder: (context, index) {
+                          final todo = state.todos[index];
+                          return Container(
+                            margin: EdgeInsets.only(bottom: 8.0)
+                          )
+                        },
+                      )
                     }
                   },
                 ),
