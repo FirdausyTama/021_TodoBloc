ef109b089557d408bc19a906f751681c385134bd
FirdausyTama <atama.firdausy@gmail.com>
2025-05-08 17:29:15 +0700

    membuat text title todo

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -112,10 +112,27 @@ class TodoPage extends StatelessWidget {
                         itemBuilder: (context, index) {
                           final todo = state.todos[index];
                           return Container(
-                            margin: EdgeInsets.only(bottom: 8.0)
-                          )
+                            margin: EdgeInsets.only(bottom: 8.0),
+                            padding: EdgeInsets.all(16.0),
+                            decoration: BoxDecoration(
+                              color: Colors.blue[50],
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  todo.title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         },
-                      )
+                      );
                     }
                   },
                 ),
